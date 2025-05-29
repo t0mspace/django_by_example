df4260d708a1bbf8a13f84788fe0236e19281167
Thomas Brenard <thomasbrenard@gmail.com>
2025-05-29 17:21:28 +0200

    add share functionnality

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="posts"),
     path("<int:year>/<int:month>/<int:day>/<slug:post>", views.detail, name="detail"),
+    path("<int:post_id>/share/", views.share, name="share"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
+from smtplib import SMTPException
+
+from django.contrib import messages
+from django.core.mail import send_mail, BadHeaderError
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 
+from .forms import EmailPostForm
 from .models import Post
 
 def index(request):
@@ -30,3 +35,47 @@ def detail(request, year, month, day, post):
   return render(request, 'blog/post/detail.html', {
     'post': post,
   })
+
+def share(request, post_id):
+  post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+  sent = False
+  if(request.method == 'POST'):
+    form = EmailPostForm(request.POST)
+    if form.is_valid():
+      safeData = form.cleaned_data
+      post_url = request.build_absolute_uri(
+        post.get_absolute_url()
+      )
+      subject = (
+        f"{safeData['name']} ({safeData['email']}) "
+        f"recommends you read {post.title}"
+      )
+      message = (
+        f"Read {post.title} at {post_url}\n\n"
+        f"{safeData['name']}\'s comments: {safeData['comments']}"
+      )
+      try:
+        send_mail(
+          subject=subject,
+          message=message,
+          from_email=None,
+          recipient_list=[safeData['to']]
+        )
+        sent = True
+      except SMTPException:
+        messages.error(request, "There was an error sending the email. Please try again later.")
+        sent = False
+
+  else:
+    form = EmailPostForm()
+
+  return render(
+    request,
+    'blog/post/share.html',
+    {
+      'post': post,
+      'form': form,
+      'sent': sent
+    }
+  )
+
